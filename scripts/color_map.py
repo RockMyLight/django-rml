cc9bd96bbfcd12d10d0f8f949dd6f5c92babdddf
Karolis Misiunas <k.misiunas@gmail.com>
2015-10-04 01:29:05 +0200

    gps color hash translator works and tested

--- scripts/color_map.py
+++ scripts/color_map.py
@@ -8,13 +8,13 @@
 #         where pos is GPS position in range [0 or 1] 
 #         where color is either 0 or 1. 
 
-doWateAtTime = -1 # some big number or -1 to skip
+doWaveAtTime = -1 # some big number or -1 to skip
 doPhaseAtTime = -1
 
-
+baseColor = (1.0,1.0,1.0) #white
 
 def toHashColor(color):
-
+  triplet = (int(color*255*baseColor[0]), int(color*255*baseColor[1]), int(color*255*baseColor[2]))
   return '#'+''.join(map(chr, triplet)).encode('hex')
 
 
