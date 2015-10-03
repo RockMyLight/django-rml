436c783cb2e7650986a935f43b8174711425c897
Karolis Misiunas <k.misiunas@gmail.com>
2015-10-04 00:37:41 +0200

    gps mapping struucture 2

--- scripts/triangulation.py
+++ scripts/triangulation.py
@@ -2,13 +2,18 @@
 #
 # needs scipy
 
-def placeOnALine( id, lon, lat ):
-   "returns relative position in range [0,1] "
+# normals
+nx = 1
+ny = 1
+offset = 0
+scale = 1
+
+def computeGPSMapper( args ):
+   "takes all know GPS locations and computes the projection on the line "
    function_suite
    return [expression]
 
 
 def placeOnALine( id, lon, lat ):
    "returns relative position in range [0,1] "
-   function_suite
-   return [expression]
+   return ((lon*nx + lat*ny) + offset)*scale
