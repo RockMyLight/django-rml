28fe61ef595652bab9bb5b8e4696850a61016159
Karolis Misiunas <k.misiunas@gmail.com>
2015-10-04 00:37:34 +0200

    gps mapping struucture

---
+++ scripts/triangulation.py
@@ -0,0 +1,14 @@
+# triangulates GPS position
+#
+# needs scipy
+
+def placeOnALine( id, lon, lat ):
+   "returns relative position in range [0,1] "
+   function_suite
+   return [expression]
+
+
+def placeOnALine( id, lon, lat ):
+   "returns relative position in range [0,1] "
+   function_suite
+   return [expression]
