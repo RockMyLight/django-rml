90f5568a55b235148ecb039287a2b4fc5bdf3559
Karolis Misiunas <k.misiunas@gmail.com>
2015-10-04 04:00:16 +0200

    gps phase

--- scripts/color_map.py
+++ scripts/color_map.py
@@ -15,7 +15,8 @@ doPhaseAtTime = -1
 
 baseColor = (1.0,1.0,1.0) #white
 
-waveSpeed = 0.1 # propagation speed for the wave
+phaseDuration = 2000
+waveDuration = 2000 # propagation speed for the wave
 
 #tested 
 def toHashColor(color):
@@ -28,7 +29,7 @@ def phase( oldlist ):
   "transforms phase"
   newlist = [0] * len(oldlist)
   for i in range(0, len(oldlist)):
-    if oldlist[i][0] > 0.5 :
+    if oldlist[i][0] > 0.5 && oldlist[i][1] > doPhaseAtTime &&  oldlist[i][1] < doPhaseAtTime + phaseDuration :
       newlist[i] = [oldlist[i][1], toHashColor( abs( oldlist[i][2]-1) ) ]
     else:
       newlist[i] = [oldlist[i][1], toHashColor( oldlist[i][2] ) ]
@@ -37,10 +38,12 @@ def phase( oldlist ):
 # not finished
 def wave( oldlist ):
   "creates wave by injecting new elements"
-  newlist = [0] * len(oldlist)
+  newlist = [] 
   for i in range(0, len(oldlist)):
-    if oldlist[i][0] > 0.5 :
+    if oldlist[i][1] > doWaveAtTime  &&  oldlist[i][1] < doWaveAtTime + waveDuration :
       newlist[i] = [oldlist[i][1], toHashColor( abs( oldlist[i][2]-1) ) ]
+
+      newlist.append( [oldlist[i][1], toHashColor( oldlist[i][2] )] )
     else:
-      newlist[i] = [oldlist[i][1], toHashColor( oldlist[i][2] ) ]
+      newlist.append( [oldlist[i][1], toHashColor( oldlist[i][2] )] )
   return newlist
