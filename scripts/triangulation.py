2bc0d6da16f4dd6b1ba42b95d3a2d216dc71f63f
Karolis Misiunas <k.misiunas@gmail.com>
2015-10-04 03:03:04 +0200

    gps latest 3

--- scripts/triangulation.py
+++ scripts/triangulation.py
@@ -11,16 +11,15 @@ demoPos = [[0.672308, 0.556435], [0.702773, 0.760115], [0.879798, 0.862075], [0.
 x = zip(*demoPos)[0]
 y = zip(*demoPos)[1]
 
-# declare variables that might be useful outside fn scope
-min_val = 0
-max_val = 0
-R = 0
 
-# takes position coordinates (lists)
+
+
+# takes position coordinates (lists) and returns [R, min_val, max_val]
 def computeGPSMapper(x,y):
   # fit a line
-	line_coef = np.polyfit(x, y, 1)
+  line_coef = np.polyfit(x, y, 1)
   theta = -math.atan2(line_coef[0], 1)
+  offset = line_coef[1];
   # translate all coerdinates lower by line_coef[1] 
   points = [[p[0], p[1] - line_coef[1]] for p in demoPos]
   # rotation matrix
@@ -30,14 +29,16 @@ def computeGPSMapper(x,y):
   rotated_x = rotated_points[0]
   min_val = min(rotated_x)
   max_val = max(rotated_x)
+  return [R, min_val, max_val]
   #final_x = [(x_val - min_val) / (max_val - min_val) for x_val in rotated_x]
 
+# setting globals
+[R, offset, min_val, max_val] = computeGPSMapper(x,y)
 
 
 def placeOnALine( id, lon, lat ):
   "returns relative position in range [0,1] "
-  rotated_points = np.dot(R, zip(*points))
-  return ((lon*nx + lat*ny) + offset)*scale
+  rotated_point = np.dot(R, [lon, lat])[0]
+  return (rotated_point - min_val) / (max_val - min_val)
+[0.556435, 0.672308]
 
-
-  
